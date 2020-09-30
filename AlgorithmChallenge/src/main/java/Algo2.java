421768d063a29eb0334b703efcbfec601a72efd7
okeyifee <okezieokechukwu94@gmail.com>
2020-09-30 08:38:53 +0100

    updated commited

---
+++ AlgorithmChallenge/src/main/java/Algo2.java
@@ -0,0 +1,9 @@
+public class Algo2{
+
+   /*
+   Breaking chocolate Algorithm
+   */
+        public static int breakChocolate(int n, int m) {
+            return Math.max(n * m - 1, 0);
+        }
+}
