504adefae36beabed1c3aedf5d16179139c4944f
okeyifee <okezieokechukwu94@gmail.com>
2020-10-01 09:57:36 +0100

    Updated Algorithms

---
+++ AlgorithmChallenge/src/main/java/Algo7.java
@@ -0,0 +1,14 @@
+/*
+Checks if a give number is even or odd
+ */
+
+public class EvenOrOdd {
+
+    public static String even_or_odd(int number) {
+
+        if (number % 2 == 0){
+            return "Even";
+        }
+        return "odd";
+    }
+}
