21292f16abcef3ace306bebb230e06d4d484b487
okeyifee <okezieokechukwu94@gmail.com>
2020-10-04 23:32:07 +0100

    updated Algorithm

--- AlgorithmChallenge/src/main/java/Algo7.java
+++ AlgorithmChallenge/src/main/java/Algo7.java
@@ -7,11 +7,10 @@ public class EvenOrOdd {
     public static String even_or_odd(int number) {
 
         /*
+        given number must be an integer
         checks if the modulus of the division between the number and 2 equals zero....
         if yes, returns even, else return odd
-
          */
-
 
         if (number % 2 == 0){
             return "Even";
