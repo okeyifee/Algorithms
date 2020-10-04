5573e817420e358c0bf62ded13940aedbd6324b4
okeyifee <okezieokechukwu94@gmail.com>
2020-10-04 23:32:44 +0100

    updated Algorithm

--- AlgorithmChallenge/src/main/java/Algo8.java
+++ AlgorithmChallenge/src/main/java/Algo8.java
@@ -4,8 +4,10 @@ Remove duplicates from an array
 
 import java.util.*;
 
+
 public class UniqueArray {
     public static int[] unique(int[] integers) {
+//        using stream output method
         return Arrays.stream(integers).distinct().toArray();
     }
 }
