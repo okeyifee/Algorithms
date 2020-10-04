fe897a0b8189db0d18878e67d126d56bdbacf5a9
okeyifee <okezieokechukwu94@gmail.com>
2020-10-04 23:28:14 +0100

    updated Algorithm

--- AlgorithmChallenge/src/main/java/Algo2.java
+++ AlgorithmChallenge/src/main/java/Algo2.java
@@ -5,6 +5,8 @@ public class Algo2{
    */
         public static int breakChocolate(int n, int m) {
 //          Returns the maximum of the operation and zero
+
             return Math.max(n * m - 1, 0);
         }
 }
+
