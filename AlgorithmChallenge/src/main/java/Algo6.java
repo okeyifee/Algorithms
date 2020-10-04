b935d6c4076de51befd2727818971a977db2d06f
okeyifee <okezieokechukwu94@gmail.com>
2020-10-04 23:31:16 +0100

    updated Algorithm

--- AlgorithmChallenge/src/main/java/Algo6.java
+++ AlgorithmChallenge/src/main/java/Algo6.java
@@ -2,8 +2,10 @@
 Convert a string to number
  */
 
+
 public class StringToNumber {
     public static int stringToNumber(String str) {
+// calls on the parseint method to convert to number
         return Integer.parseInt(str);
     }
 }
