650795a114d96d8ee68883bdd776f8fde7ea0a05
okeyifee <okezieokechukwu94@gmail.com>
2020-10-13 22:58:49 +0100

    initial commit

--- AlgorithmChallenge/src/main/java/Algo2.java
+++ AlgorithmChallenge/src/main/java/Algo2.java
@@ -6,7 +6,6 @@ public class Algo2{
         public static int breakChocolate(int n, int m) {
           Returns the maximum of the operation and zero
 
-
             return Math.max(n * m - 1, 0);
         }
 }
