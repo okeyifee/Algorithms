cbb5f44de8cd365b634d69dc0b9f5c97ea1f4d5f
okeyifee <okezieokechukwu94@gmail.com>
2020-10-13 22:56:36 +0100

    initial commit

--- AlgorithmChallenge/src/main/java/Algo8.java
+++ AlgorithmChallenge/src/main/java/Algo8.java
@@ -6,7 +6,9 @@ import java.util.*;
 
 
 public class UniqueArray {
+
     public static int[] unique(int[] integers) {
+
 //        using stream output method
         return Arrays.stream(integers).distinct().toArray();
     }
